844e7bc7e89107e94765c436f88e37b3fa5771e6
coder-suzan <79319220+coder-suzan@users.noreply.github.com>
2021-05-13 00:03:07 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function imgSlider(anyImg){
+    document.querySelector("#myImg").src = anyImg;
+}
+
+function changeHeaderBg(color){
+    const header = document.querySelector(".myHeader");
+    header.style.background = color;
+}
